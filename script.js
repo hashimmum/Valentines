6e645a6adfeb0d602db69191423cce9e2c606640
hashimmum <144855436+hashimmum@users.noreply.github.com>
2024-02-03 17:55:31 -0800

    script.js

--- script.js
+++ script.js
@@ -9,23 +9,22 @@ let yesClickedOnce = false;
 yesbtn.addEventListener("click", () => {
     if (!yesClickedOnce) {
         question.innerHTML = "Do you want to be my Valentines?";
-        gif.src = "second.gif"; // Corrected from 'scr' to 'src'
+        gif.src = "second.gif";
         yesClickedOnce = true;
     } else {
         question.innerHTML = "I LOVE YOU!! made with love by hashim!";
-        gif.src = "third.gif"; // Corrected from 'scr' to 'src'
+        gif.src = "third.gif"; 
     }
 });
 
 nobtn.addEventListener("mouseover", () => {
     const nobtnRect = nobtn.getBoundingClientRect();
     const maxX = window.innerWidth - nobtnRect.width;
-    const maxY = window.innerHeight - nobtnRect.height; // Corrected from 'innderHeight'
-
-    const randomX = Math.floor(Math.random() * maxX); // Corrected method call
-    const randomY = Math.floor(Math.random() * maxY); // Corrected method call
+    const maxY = window.innerHeight - nobtnRect.height; 
+    const randomX = Math.floor(Math.random() * maxX); 
+    const randomY = Math.floor(Math.random() * maxY); 
 
     nobtn.style.left = randomX + 'px';
     nobtn.style.top = randomY + 'px';
-    nobtn.style.position = 'absolute'; // Ensure your CSS accommodates this.
+    nobtn.style.position = 'absolute'; 
 });
